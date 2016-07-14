24b70d21218776b549d9a35f2d123763ab03c86e
Tull Gearreald <tull.gearreald@avant.com>
2016-07-14 13:59:13 -0500

    Migration to shared address table format

---
+++ src/main/resources/db/migration/V201607141330__add_sharable_addresses_support.sql
@@ -0,0 +1,38 @@
+--Create contact_address Sharing Table
+CREATE TABLE contact_addresses(
+	id INTEGER PRIMARY KEY NOT NULL,
+	contact_id INTEGER NOT NULL,
+  	address_id INTEGER NOT NULL,
+  	current_address BOOLEAN NOT NULL
+);
+
+-- Migrate from old method
+INSERT INTO contact_addresses (contact_id,address_id,current_address)
+SELECT c.id AS contact_id
+	,a.id AS address_id
+	,a.active AS current_address
+FROM contacts AS c
+INNER JOIN addresses AS a
+	ON c.id=a.contact_id;
+
+--rename old address table	
+ALTER TABLE addresses RENAME TO addresses_old;
+
+--Create new address table
+CREATE TABLE addresses(
+	id INTEGER PRIMARY KEY NOT NULL,
+	contact_id INTEGER NOT NULL,
+	street VARCHAR(255),
+	apartment VARCHAR(255),
+	zip_code VARCHAR(255),
+	city VARCHAR(255),
+	state VARCHAR(255),
+	country VARCHAR(255)
+);
+-- Copy old data
+INSERT INTO addresses (id,contact_id,street,apartment,zip_code,city,state,country)
+SELECT id,contact_id,street,apartment,zip_code,city,state,country FROM addresses_old;
+
+-- Drop old table
+
+DROP TABLE addresses_old;
