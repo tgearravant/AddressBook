0900f4fb903c1b32ea955d83eabdfaa54eb3b101
Tull Gearreald <tull.gearreald@avant.com>
2016-08-23 12:20:37 -0500

    Moved Tests around.

--- src/test/java/net/tullco/addressbook/test_suites/UtilTests.java
+++ src/test/java/net/tullco/addressbook/test_suites/UtilTests.java
@@ -7,9 +7,10 @@ import org.junit.runners.Suite.SuiteClasses;
 import net.tullco.addressbook.test_classes.LocaleUtilTest;
 import net.tullco.addressbook.test_classes.SQLUtilTest;
 import net.tullco.addressbook.test_classes.SystemUtilTest;
+import net.tullco.addressbook.test_classes.ViewUtilTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ SQLUtilTest.class, SystemUtilTest.class, LocaleUtilTest.class })
+@SuiteClasses({ SQLUtilTest.class, SystemUtilTest.class, LocaleUtilTest.class, ViewUtilTest.class })
 public class UtilTests {
 
 }
